9e9bcf2397fdef2e91a3613732f261d46f6426a7
Sebastian Pardo <spardo83@gmail.com>
2017-08-29 22:41:01 -0300

    Commit Inicial

---
+++ src/main/java/ar/edu/unlam/diit/scaw/entities/Usuario.java
@@ -0,0 +1,58 @@
+package ar.edu.unlam.diit.scaw.entities;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String email;
+	private String contraseña;
+	private String apellido;
+	private String nombre;
+
+	public Usuario() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+}
